a12f2876cf41d6a1f5436105e018659c7b64b36c
Fawa24 <ofeglol@gmail.com>
2024-06-09 14:57:24 +0300

    Finish Infrastructure implementation

---
+++ API/Core/Core/EnviromentVariables.cs
@@ -0,0 +1,7 @@
+﻿namespace Core
+{
+	public static class EnviromentVariables
+	{
+		public const string DefaultConnection = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=EmergencyNotificator;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
+	}
+}
