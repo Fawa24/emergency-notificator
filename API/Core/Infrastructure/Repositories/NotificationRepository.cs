9e5f515232ec4734a18a36e2da01e3473d4629b4
Fawa24 <ofeglol@gmail.com>
2024-06-09 14:52:44 +0300

    Implement NotificationRepository

---
+++ API/Core/Infrastructure/Repositories/NotificationRepository.cs
@@ -0,0 +1,43 @@
+﻿using Core.Domain.RepositoriesContracts;
+using Core.DTO;
+using Infrastructure.DbContexts;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Repositories
+{
+	public class NotificationRepository : INotificationRepository
+	{
+		private readonly ApplicationDbContext _db;
+
+		public NotificationRepository(ApplicationDbContext db)
+		{
+			_db = db;
+		}
+
+		public async Task AddNotification(Notification notification)
+		{
+			await _db.Notifications.AddAsync(notification);
+			await _db.SaveChangesAsync();
+		}
+
+		public async Task<List<Notification>> GetUsersNotifications(string username)
+		{
+			return await _db.Notifications
+				.Include(nameof(Notification.Sender))
+				.Include(nameof(Notification.Recipient))
+				.Where(n => n.Recipient.UserName.Equals(username))
+				.ToListAsync();
+		}
+
+		public Notification ToNotification(AddNotificationDTO request)
+		{
+			return new Notification()
+			{
+				NotificationId = Guid.NewGuid(),
+				Message = request.Message,
+				SenderId = _db.Users.FirstOrDefault(user => user.UserName == request.Sender).Id,
+				RecipientId = _db.Users.FirstOrDefault(user => user.UserName == request.Sender).Id
+			};
+		}
+	}
+}
