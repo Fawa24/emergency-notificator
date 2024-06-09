b3520b74b47a5fd7f8111f690109d928fae7a309
Fawa24 <ofeglol@gmail.com>
2024-06-09 13:13:52 +0300

    Implement contract for a NotificationRepository

---
+++ API/Core/Core/Domain/RepositoriesContracts/INotificationRepository.cs
@@ -0,0 +1,21 @@
+﻿namespace Core.Domain.RepositoriesContracts
+{
+	/// <summary>
+	/// NotificationRepository contract
+	/// </summary>
+	public interface INotificationRepository
+	{
+		/// <summary>
+		/// Adds new notification to the data source
+		/// </summary>
+		/// <param name="notification">Notification to add</param>
+		/// <returns></returns>
+		Task AddNotification(Notification notification);
+		/// <summary>
+		/// Returns notifications for a concrete user
+		/// </summary>
+		/// <param name="username">Username to search</param>
+		/// <returns>All the notifications for provided user</returns>
+		Task<List<Notification>> GetUsersNotifications(string username);
+	}
+}
