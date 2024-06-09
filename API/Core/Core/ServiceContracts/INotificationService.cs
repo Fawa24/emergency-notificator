ca321fe6cb9cd64da52181b2829772e4656efc77
Fawa24 <ofeglol@gmail.com>
2024-06-09 13:30:03 +0300

    Implement NotificationService contract

---
+++ API/Core/Core/ServiceContracts/INotificationService.cs
@@ -0,0 +1,16 @@
+﻿using Core.DTO;
+
+namespace Core.ServiceContracts
+{
+	/// <summary>
+	/// NotificationService contract
+	/// </summary>
+	public interface INotificationService
+	{
+		/// <summary>
+		/// Sends notification request to the NotificationProcessor through RabbitMQ
+		/// </summary>
+		/// <param name="request">Request to send</param>
+		void SendNotificationRequest(NotificationRequest request);
+	}
+}
