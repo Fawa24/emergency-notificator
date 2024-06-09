aba11c31fd5b17126d385635550d2a81220fa32d
Fawa24 <ofeglol@gmail.com>
2024-06-09 13:25:02 +0300

    Implement NotificationRequestDTO and NotificationRequest

---
+++ API/Core/Core/DTO/NotificationRequest.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Core.DTO
+{
+	public class NotificationRequest
+	{
+		public string Sender { get; set; } = null!;
+
+		public List<string> Recipients { get; set; } = null!;
+
+		[Required(ErrorMessage = "Title can not be empty")]
+		public string Title { get; set; } = null!;
+
+		[Required(ErrorMessage = "Message text can not be empty")]
+		public string Message { get; set; } = null!;
+	}
+}
---
+++ API/Core/Core/DTO/NotificationRequestDTO.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Core.DTO
+{
+	public class NotificationRequestDTO
+	{
+		public List<string> Recipients { get; set; } = null!;
+
+		[Required(ErrorMessage = "Title can not be empty")]
+		public string Title { get; set; } = null!;
+
+		[Required(ErrorMessage = "Message text can not be empty")]
+		public string Message { get; set; } = null!;
+
+		public NotificationRequest ToNotificationRequest(string username)
+		{
+			return new NotificationRequest()
+			{
+				Sender = username,
+				Recipients = Recipients,
+				Title = Title,
+				Message = Message
+			};
+		}
+	}
+}
