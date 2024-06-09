83f895dc3e47923557f8651f1a5d6e6e8e21481d
Fawa24 <ofeglol@gmail.com>
2024-06-09 14:33:50 +0300

    Implement ApplicationDbContext

---
+++ API/Core/Infrastructure/DbContexts/ApplicationDbContext.cs
@@ -0,0 +1,44 @@
+﻿using Core;
+using Core.Domain.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.DbContexts
+{
+	public class ApplicationDbContext : IdentityDbContext<ApplicationUser, ApplicationRole, Guid>
+	{
+		public DbSet<Notification> Notifications { get; set; }
+
+		public ApplicationDbContext(DbContextOptions options) : base(options) { }
+
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			if (!optionsBuilder.IsConfigured)
+			{
+				optionsBuilder.UseSqlServer(EnviromentVariables.DefaultConnection);
+			}
+		}
+
+		protected override void OnModelCreating(ModelBuilder builder)
+		{
+			base.OnModelCreating(builder);
+
+			builder.Entity<Notification>(entity =>
+			{
+				entity.HasOne(n => n.Sender)
+					.WithMany()
+					.HasForeignKey(n => n.SenderId)
+					.OnDelete(DeleteBehavior.Restrict)
+					.IsRequired();
+
+				entity.HasOne(n => n.Recipient)
+					.WithMany()
+					.HasForeignKey(n => n.RecipientId)
+					.OnDelete(DeleteBehavior.Restrict)
+					.IsRequired();
+			});
+		}
+
+
+	}
+}
