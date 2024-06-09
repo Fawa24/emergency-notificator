91779d1cadc7819c9a71cf57c1364d7a4582ae09
Fawa24 <ofeglol@gmail.com>
2024-06-09 12:57:50 +0300

    Add Identity package + implement Identity entities

---
+++ API/Core/Core/Domain/Entities/ApplicationUser.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace Core.Domain.Entities
+{
+	public class ApplicationUser : IdentityUser<Guid>
+	{
+		[Required]
+		public string FullName { get; set; } = null!;
+	}
+}
